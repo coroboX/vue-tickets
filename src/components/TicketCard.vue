9279f1d2098fa01852f56d833730e57996b003a5
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-09 12:52:57 +0300

    add logo + general styles

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="ticket">
     <div class="ticket__action">
-      <logo class="ticket__saller-logo"></logo>
+      <img
+        class="ticket__airline-logo"
+        src="./../assets/turkish-airlines-logo-sm.png"
+      />
       <button class="button ticket__button">
         <p class="button__description">
           Купить
@@ -26,14 +29,27 @@ export default {
   .ticket {
     height: 161px;
     width: 566px;
+    margin: 50px auto 0;
     display: flex;
 
     border-radius: 5px;
     background-color: #fff;
+    border: 1px solid pink;
 
     &__action{ 
       width: 200px;
-      height: 100%; 
+      height: 100%;
+      padding: 25px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+
+      border-right: 1px solid #eceff1;
+    }
+
+    &__airline-logo {
+      width: 120px;
     }
 
     &__button {
