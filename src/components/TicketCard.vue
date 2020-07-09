3e0b51561f296c6106ff0f1f4b18b2669b02e6b5
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-09 18:00:34 +0300

    add arrow plane

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -38,6 +38,10 @@
         </p>
       </div>
       <div class="ticket__stops">
+        <span class="ticket__stops-text">
+          2 Пересадки
+        </span>
+        <div class="ticket__arrow"></div>
       </div>
     </div>
   </div>
@@ -51,8 +55,8 @@ export default {
 
 <style scoped lang="scss">
   .ticket {
-    height: 161px;
     width: 566px;
+    height: 161px;
     margin: 50px auto 0;
     display: flex;
 
@@ -64,6 +68,7 @@ export default {
       width: 200px;
       height: 100%;
       padding: 25px 0;
+
       display: flex;
       flex-direction: column;
       justify-content: space-between;
@@ -133,7 +138,42 @@ export default {
     }
 
     &__stops {
+      width: 110px;
+
       position: absolute;
+      top: 24px;
+      left: 50%;
+
+      transform: translateX(-50%);
+    }
+
+    &__stops-text {
+      display: inline-block;
+      width: 100%;
+
+      color: #8b9497;
+      font-size: 10px;
+      text-align: center;
+    }
+
+    &__arrow {
+      width: 100%;
+      height: 1px;
+
+      background-color: #d2d5d6;
+    }
+
+    &__arrow::after {
+      content: '';
+      display: block;
+      width: 15px;
+      height: 13px;
+      position: absolute;
+      right: 0;
+      transform: translateY(-6px);
+
+      background: #fff no-repeat center;
+      background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='13' height='13' viewBox='0 0 13 13' fill='%23d2d5d6'><path d='M3.9 13H5.2L8.45 7.52632L12.025 7.52632C12.5645 7.52632 13 7.06789 13 6.5C13 5.93211 12.5645 5.47368 12.025 5.47368L8.45 5.47368L5.2 0L3.9 0L5.525 5.47368H1.95L0.975 4.10526L0 4.10526L0.65 6.5L0 8.89474H0.975L1.95 7.52632H5.525L3.9 13Z'/></svg>");
     }
   }
 
