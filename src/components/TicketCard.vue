17ceb9b716152dd9d5e0ad74a61e618df1a54f78
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-07-09 12:26:12 +0300

    started ticket card markup

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="ticket">
+    <div class="ticket__action">
+      <logo class="ticket__saller-logo"></logo>
+      <button class="button ticket__button">
+        <p class="button__description">
+          Купить
+        </p>
+        <p class="button__price">
+          за 20 056 ₽
+        </p>
+      </button>
+    </div>
+
+
   </div>
 </template>
 
@@ -10,6 +23,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  .ticket {
+    height: 161px;
+    width: 566px;
+    display: flex;
 
+    border-radius: 5px;
+    background-color: #fff;
+
+    &__action{ 
+      width: 200px;
+      height: 100%; 
+    }
+
+    &__button {
+      color: #fff;
+      font-family: "Open Sans", Helvetica, sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 22px;
+
+      width: 160px;
+      height: 56px;
+      text-align: center;
+
+      background-color: #ff6d00;
+      border: none;
+      border-radius: 5px;
+    }
+
+    &__button:hover {
+      background-color: #ff8124;
+      cursor: pointer;
+    }
+  }
 
 </style>
